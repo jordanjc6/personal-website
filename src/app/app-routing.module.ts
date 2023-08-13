37a9ec47a9a04b481b7ad4692e6d16f0b4f1965f
jordanjc6 <jordanjcastro8@gmail.com>
2023-08-13 16:39:14 -0400

    finish header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,23 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router'
 
 import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: '/home',
+    redirectTo: 'home',
   },
   {
     path: 'home', component: HomeComponent
+  },
+  {
+    path: 'projects', component: ProjectsComponent
+  },
+  {
+    path: 'contact', component: ContactComponent
   }
 ]
 
